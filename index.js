eda7fc8448b674295bfce56301b65d98c6772f05
mmrozewski <mmrozews@gmail.com>
2020-04-07 11:06:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+
+function findMatching(array, string) {
+
+    return array.filter(driver => {return driver.toUpperCase() === string.toUpperCase()})
+}
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby', "bobby"]
+
+function fuzzyMatch(array, string) {
+    return array.filter(driver => {return driver.startsWith(string)})
+}
+
+function matchName(array, string) {
+    return array.filter(driver => {return driver.name === string})
+}
